9f7d7a28e8c25a2dab62f99ef58ce7592e94967f
Anthony Li <lisang881021@sina.com>
2020-04-21 23:45:06 +0800

    model:add role

---
+++ server/models/role.js
@@ -0,0 +1,62 @@
+'use strict'
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER(20).UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      userName: {
+        type: DataTypes.STRING(45),
+        field: 'user_name'
+      },
+      userNamePinyin: {
+        type: DataTypes.STRING(45),
+        field: 'user_en_name'
+      },
+      userAliasName: {
+        type: DataTypes.STRING(45),
+        field: 'user_alias_name'
+      },
+      userPassword: {
+        type: DataTypes.STRING(45),
+        field: 'user_password'
+      },
+      userTel: {
+        type: DataTypes.STRING(20),
+        field: 'user_tel'
+      },
+      userAddress: {
+        type: DataTypes.STRING(100),
+        field: 'user_address'
+      },
+      userSex: {
+        type: DataTypes.STRING(10),
+        field: 'user_sex'
+      },
+      status: {
+        type: DataTypes.STRING(10),
+        field: 'status'
+      },
+      finance: {
+        type: DataTypes.STRING(10),
+        field: 'finance'
+      },
+      createTime: {
+        type: DataTypes.DATE,
+        field: 'create_time'
+      },
+      updateTime: {
+        type: DataTypes.DATE,
+        field: 'update_time'
+      }
+    },
+    {
+      tableName: 'user'
+    }
+  )
+
+  return User
+}
