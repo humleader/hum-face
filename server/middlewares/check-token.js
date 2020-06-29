268f31660cf58c05cea1fc5cda97597dfb9becd6
Anthony Li <lisang881021@sina.com>
2020-06-29 23:57:40 +0800

    fix:删除文件

--- server/middlewares/check-token.js
+++ server/middlewares/check-token.js
@@ -1,14 +1,10 @@
 module.exports = function() {
   return async function(ctx, next) {
-    if (ctx.path.startsWith('/api') && ctx.path.indexOf('api/user/login') === -1) {
-      if (!ctx.session.user) {
-        ctx.status = 401
-        ctx.body = {
-          code: 1,
-          error: '用户登录已失效'
-        }
-      } else {
-        await next()
+    if (!ctx.session) {
+      ctx.status = 401
+      ctx.body = {
+        code: 1,
+        error: '用户登录已失效'
       }
     } else {
       await next()
